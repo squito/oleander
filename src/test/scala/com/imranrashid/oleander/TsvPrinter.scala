686ef81038b02ada0a6b5be26576eb5e900c9622
Imran Rashid <imran@quantifind.com>
2013-06-20 09:31:13 -0700

    add TsvPrinter to make it easy to generate boxplots in R

---
+++ src/test/scala/com/imranrashid/oleander/TsvPrinter.scala
@@ -0,0 +1,41 @@
+package com.imranrashid.oleander
+
+import ichi.bench.Thyme.Benched
+import java.io.{PrintWriter, File}
+import ichi.bench.Thyme
+
+
+/**
+ *
+ */
+class TsvPrinter(val th: Thyme, val file: File) {
+  val out = new PrintWriter(file)
+  out.println("title\truntime")
+
+  def addToTsv(benched: Benched) {
+    benched.runtimeResults.data.value.foreach{runtime =>
+      out.println(benched.title + "\t" + (runtime / benched.runtimeEffort))
+    }
+  }
+
+  def close() {out.close()}
+
+
+  def tsvBench[A](f: => A, effort: Int = 1, title: String = "") = {
+    val br = Thyme.Benched.empty
+    br.title = title
+    val ans = th.bench(f)(br, effort = effort)
+    println(br)
+    addToTsv(br)
+    ans
+  }
+
+  def showRCommand = println(
+    """read.table(""" + file.getAbsolutePath +
+      """, sep="\t",h=T)
+        |# you might want to reorder the plot with something like:
+        |# data$title = factor(data$title, levels = c(<desired order>), ordered=T)
+        |boxplot(runtime ~ title, data, ylab="seconds")
+      """.stripMargin
+  )
+}
