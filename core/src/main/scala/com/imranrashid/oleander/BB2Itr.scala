4313557081eebf6dfd618e8d2d4234664e3dd07b
Imran Rashid <imran@quantifind.com>
2013-12-30 17:31:42 -0600

    more utils for working with byte buffer backed stuff ...

--- core/src/main/scala/com/imranrashid/oleander/BB2Itr.scala
+++ core/src/main/scala/com/imranrashid/oleander/BB2Itr.scala
@@ -10,17 +10,47 @@ import java.nio.ByteBuffer
  */
 object BB2Itr {
   def fromMemMappedFile[T <: BB2: ClassTag](file: File): Iterator[T] = {
-    //TODO auto-create a T
-    val cls = implicitly[ClassTag[T]].runtimeClass
-    val ctor = cls.getConstructor(Array(classOf[ByteBuffer], Integer.TYPE): _*)
     val raf = new RandomAccessFile(file, "rw")
     val fc = raf.getChannel
     val maxBytes = file.length
     val mbb = fc.map(MapMode.READ_WRITE, 0, maxBytes)
-    val t = ctor.newInstance(Array(mbb, new java.lang.Integer(0)): _*).asInstanceOf[T]
+    val t = makeBB2[T](mbb)
     (0 until maxBytes.toInt by t.numBytes).iterator.map{ idx =>
       t.setBuffer(mbb, idx)
       t
     }
+  }
+
+  def bbAsTraversable[T <: BB2](bb: ByteBuffer, obj: T)= {
+    new BB2Traversable(bb, 0, bb.limit(), obj)
+  }
+
+  class BB2Traversable[T <: BB2](bb: ByteBuffer, start: Int, length: Int, o: T) extends Traversable[T] {
+    def foreach[R](f: T => R){
+      (start to (start + length) by o.numBytes).foreach{idx =>
+        o.setBuffer(bb, idx)
+        f(o)
+      }
+    }
+  }
+
+  def makeBB2[T <: BB2: ClassTag](bb: ByteBuffer): T = {
+    val cls = implicitly[ClassTag[T]].runtimeClass
+    val ctor = cls.getConstructor(Array(classOf[ByteBuffer], Integer.TYPE): _*)
+    ctor.newInstance(Array(bb, new java.lang.Integer(0)): _*).asInstanceOf[T]
+  }
+
+  def indexedBB2[T <: BB2: ClassTag](bb: ByteBuffer): IndexedBb2[T] = {
+    val t = makeBB2[T](bb)
+    new IndexedBb2[T](bb, 0, bb.limit(), t)
+  }
+
+}
+
+class IndexedBb2[T <: BB2](bb: ByteBuffer, start: Int, length: Int, o: T) {
+
+  def apply(idx: Int): T = {
+    o.setBuffer(bb, start + idx * o.numBytes)
+    o
   }
 }
